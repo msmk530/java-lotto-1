57161a6231e202d526330db59787cea524222b14
kimminsub <msmk530@naver.com>
2020-04-29 17:29:24 +0900

    test : 수동매수 최소와 최대 테스트

--- src/test/java/domain/ValidatorTest.java
+++ src/test/java/domain/ValidatorTest.java
@@ -1,5 +1,7 @@
 package domain;
 
+import exception.ManualLotto.MaximumCountOfManualLottoException;
+import exception.ManualLotto.MinimumCountOfManualLottoException;
 import exception.PurchasePrice.MinimumPurchasePriceException;
 import exception.PurchasePrice.PurchasePriceUnitException;
 import org.junit.jupiter.api.Test;
@@ -11,12 +13,24 @@ class ValidatorTest {
     @Test
     void 최소구매금액_만족_테스트() {
         assertThatExceptionOfType(MinimumPurchasePriceException.class)
-                .isThrownBy(()->Validator.isCorrectPurchasePrice(900));
+                .isThrownBy(() -> Validator.isCorrectPurchasePrice(900));
     }
 
     @Test
     void 구매금액_단위_테스트() {
         assertThatExceptionOfType(PurchasePriceUnitException.class)
-                .isThrownBy(()->Validator.isCorrectPurchasePrice(1010));
+                .isThrownBy(() -> Validator.isCorrectPurchasePrice(1010));
+    }
+
+    @Test
+    void 수동매수_최소_테스트() {
+        assertThatExceptionOfType(MinimumCountOfManualLottoException.class)
+                .isThrownBy(() -> Validator.isCorrectCountOfManualLotto(5, -1));
+    }
+
+    @Test
+    void 수동매수_최대_테스트() {
+        assertThatExceptionOfType(MaximumCountOfManualLottoException.class)
+                .isThrownBy(() -> Validator.isCorrectCountOfManualLotto(5, 6));
     }
 }
