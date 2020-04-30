eb954cb1be9c9618b313740b613c97a795b1c24e
kimminsub <msmk530@naver.com>
2020-04-30 16:02:22 +0900

    feat : 로또생성자 테스트

---
+++ src/test/java/domain/LottoTest.java
@@ -0,0 +1,17 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LottoTest {
+    @Test
+    void 생성자_테스트() {
+        Lotto lotto = new Lotto(Arrays.asList(1,2,3,4,5,6));
+        assertThat(lotto).isNotNull();
+    }
+
+}
